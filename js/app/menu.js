d5b5e82925bbc63d8a151eba7b6a14d3a9d405dc
Pablo Perez <pdperez@outlook.com>
2014-11-23 06:49:37 -0800

    consolidated media queries for responsive side menu and modified javascript to close side menu when clicking outside of it

--- js/app/menu.js
+++ js/app/menu.js
@@ -1,6 +1,24 @@
-$( document ).ready(function() {
-	$( ".menu-toggle" ).on( "click", function( event ) {
-	    $(".transformer").toggleClass( "is-open");
+
+(function($){
+	
+	var transformer=$(".transformer");
+	
+	//toggle side menu when clicking on menu-toggle button
+	$( document ).ready(function() {
+		$(".menu-toggle" ).on( "click", function( event ) {
+		    event.preventDefault();
+			transformer.toggleClass("is-open");
+		});//menu-toggle on click
+	});//document ready
+	
+	//dismiss opened side menu when clicking outside of menu
+	$(document).on('click', function(event) {
+		if ($(event.target).closest(".main-inner").length) {
+			if(transformer.hasClass('is-open')){
+				transformer.removeClass("is-open");
+			}//if transformer is-open
+		}//if event.target closest
 	});
-   
-});
+	
+}(jQuery));//anonymous closure
+
