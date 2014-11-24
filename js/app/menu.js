766efbab6503c37d626ccd8467bab0712a88de20
Pablo Perez <pdperez@outlook.com>
2014-11-23 20:36:10 -0800

    made changes to fix the side menu transition when closing

--- js/app/menu.js
+++ js/app/menu.js
@@ -4,12 +4,10 @@
 	var transformer=$(".transformer");
 	
 	//toggle side menu when clicking on menu-toggle button
-	$( document ).ready(function() {
-		$(".menu-toggle" ).on( "click", function( event ) {
-		    event.preventDefault();
-			transformer.toggleClass("is-open");
-		});//menu-toggle on click
-	});//document ready
+	$(".menu-toggle" ).on( "click", function( event ) {
+	    event.preventDefault();
+		transformer.toggleClass("is-open");
+	});//menu-toggle on click
 	
 	//dismiss opened side menu when clicking outside of menu
 	$(document).on('click', function(event) {
@@ -18,7 +16,7 @@
 				transformer.removeClass("is-open");
 			}//if transformer is-open
 		}//if event.target closest
-	});
+	});//document on click
 	
 }(jQuery));//anonymous closure
 
