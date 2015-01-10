e90de368ca90299bf45da6ee74fa31f486c31dfc
Pablo Perez <pdperez@outlook.com>
2015-01-10 05:11:42 -0800

    fixed again responsive navigation bar because I was having problem with the body not overflowing

--- js/app.js
+++ js/app.js
@@ -1,25 +1,23 @@
 
 (function($){
 	
-	var offCanvas=$("#top-bar"),
-		main=$("#main"),
-		toggle=$("#menu-toggle");
+	var main=$("#main"),
+		toggle=$("#menu-toggle"),
+		wrapper=$("#wrapper");
 	
 
-	//toggle side menu when clicking on menu-toggle button
+	//toggle .is-open class when clicking on menu-toggle button
 	toggle.on( "click", function( event ) {
-		offCanvas.toggleClass("is-open");
-		main.toggleClass("is-open");
+		wrapper.toggleClass("is-open");
 	});//menu-toggle on click
 
 	//dismiss opened side menu when clicking outside of menu
 	$(document).on('click', function(event) {
 		if ($(event.target).closest("#main").length && 
 		!$(event.target).is(toggle)) {
-			if(offCanvas.hasClass('is-open')){
-				offCanvas.removeClass("is-open");
-				main.removeClass("is-open");
-			}//if offCanvas is-open, remove class
+			if(wrapper.hasClass('is-open')){
+				wrapper.removeClass("is-open");
+			}//if wrapper is-open, remove class
 		}//if event.target closest and != toggle
 	});//document on click
 	
