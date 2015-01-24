cf93b31771bc61148d8a07d2163b26fb0ec013db
Pablo Perez <pdperez@outlook.com>
2015-01-23 18:54:43 -0800

    changed menu.js to add progressive enhancement features

--- js/app/menu.js
+++ js/app/menu.js
@@ -2,23 +2,45 @@
 (function($){
 	
 	var main=$("#main"),
-		toggle=$("#menu-toggle"),
-		wrapper=$("#wrapper");
+		toggleButton=$("#menu-toggle"),
+		wrapper=$("#wrapper"),
+		topBar=$("#top-bar")
+		navLinks=$(".nav-links"),
+		mql=window.matchMedia("screen and (max-width: 800px)");//responsive menu media 		breakpoint
 	
-	//toggle .is-open class when clicking on menu-toggle button
-	toggle.on( "click", function( event ) {
-		wrapper.toggleClass("is-open");
-	});//menu-toggle on click
+		//add responsive classes to topBar and navLinks elements
+		//menu only adopts responsive form if javascript has loaded 
+		//and added these classes
+		//also display toggleButton
+		$(window).load(function(){
+			if (topBar && navLinks){
+				topBar.addClass("top-bar-resp");
+				navLinks.addClass("nav-links-resp");
+				$(window).resize(function(){//if media query is true
+					if (mql.matches){
+					toggleButton.show();
+					}//display toggleButton at breakpoint
+					else {//if media query not true
+						toggleButton.hide();//hide toggle button
+					}
+				});
+			}
+		});//window load
+	
+		//toggle .is-open class when clicking on menu-toggle button
+		toggleButton.on( "click", function( event ) {
+			wrapper.toggleClass("is-open");
+		});//menu-toggle on click
 
-	//dismiss opened side menu when clicking outside of menu
-	$(document).on('click', function(event) {
-		if ($(event.target).closest("#main").length && 
-		!$(event.target).is(toggle)) {
-			if(wrapper.hasClass('is-open')){
-				wrapper.removeClass("is-open");
-			}//if wrapper is-open, remove class
-		}//if event.target closest and != toggle
-	});//document on click
+		//dismiss opened side menu when clicking outside of menu
+		$(document).on('click', function(event) {
+			if ($(event.target).closest("#main").length && 
+			!$(event.target).is(toggle)) {
+				if(wrapper.hasClass('is-open')){
+					wrapper.removeClass("is-open");
+				}//if wrapper is-open, remove class
+			}//if event.target closest and != toggle
+		});//document on click
 	
 }(jQuery));//anonymous closure
 
