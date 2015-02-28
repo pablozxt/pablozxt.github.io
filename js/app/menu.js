8b78f140c088be3a28997178d283d0c9a751e325
Pablo Perez <pdperez@outlook.com>
2015-02-28 06:00:33 -0800

    added condition to test for css transforms 3d to menu.js

--- js/app/menu.js
+++ js/app/menu.js
@@ -1,27 +1,31 @@
 
 (function ($) {
 	
-	"use strict";
-	
+    "use strict";
+   
     var main = $('#main'),
 		toggleButton = $('#menu-toggle'),
 		wrapper = $('#wrapper'),
 		topBar = $('#top-bar'),
 		navLinks = $('.nav-links'),
+		html = $('html'),
 		mediaQuery = 'screen and (max-width: 800px)',//responsive menu breakpoint
 		mql = window.matchMedia(mediaQuery);//media query list
-	
-		//function allows to toggle an element if a media
-		//query list is true or false
-		function toggleResp(mediaQueryList,element) {
-		    if (mediaQueryList.matches) {
-				element.show();
-			}//display element if breakpoint is met
-			else {
-				element.hide();
-			}//hide element if breakpoint is not met
-		}
 		
+	//function allows to toggle an element if a media
+	//query list is true or false
+	function toggleResp(mediaQueryList,element) {
+	    if (mediaQueryList.matches) {
+			element.show();
+		}//display element if breakpoint is met
+		else {
+			element.hide();
+		}//hide element if breakpoint is not met
+	}
+	
+	//if modernizr detects csstransforms3d, then add off-canvas menu
+	if (html.hasClass('csstransforms3d')) { 
+	
 		//add responsive classes to topBar and navLinks elements
 		//menu only adopts responsive form if javascript has loaded 
 		//and added these classes
@@ -32,12 +36,12 @@
 			}//if topBar and navlinks
 			toggleResp(mql,toggleButton);
 		});//window load
-		
+	
 		//display toggleButton if max-width < breakpoint
 		$(window).resize(function () {
 			toggleResp(mql,toggleButton);
 		});//on window resize function
-	
+
 		//toggle .is-open class when clicking on menu-toggle button
 		toggleButton.on( 'click', function() {
 			wrapper.toggleClass('is-open');
@@ -54,6 +58,7 @@
 				}//if wrapper is-open, remove class
 			}//if event.target closest and != toggle
 		});//document on click
-	
-}(jQuery));//immediately invoked function
+	}//if html has class 3d transform
+		
+}(jQuery));//close immediately invoked function expression
 
