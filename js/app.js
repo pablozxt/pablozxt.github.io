fedcd47e485b45e2d4673d51c8b20f702c366914
Pablo Perez <pdperez@outlook.com>
2015-03-28 06:28:08 -0700

    multiple changes to navigation system to switch from a list style navigation to a paragraph style navigation. Navigation works differently in the home page than in the rest of the pages

--- js/app.js
+++ js/app.js
@@ -6,8 +6,7 @@
     var main = $('#main'),
 		toggleButton = $('#menu-toggle'),
 		wrapper = $('#wrapper'),
-		topBar = $('#top-bar'),
-		navLinks = $('.nav-links'),
+		nav = $('#navigation'),
 		html = $('html'),
 		mediaQuery = 'screen and (max-width: 44.44444em)',//responsive menu breakpoint
 		mql = window.matchMedia(mediaQuery);//media query list
@@ -28,40 +27,42 @@
 	//then add off-canvas menu
 	if (html.hasClass('csstransforms3d') && (window.matchMedia)) { 
 	
-		//add responsive classes to topBar and navLinks elements
+		//add responsive classes to nav and navLinks elements
 		//menu only adopts responsive form if javascript has loaded 
 		//and added these classes
-		$(window).load(function () {
-			if (topBar && navLinks) {
-				topBar.addClass('top-bar-resp');
-				navLinks.addClass('nav-links-resp');	
-			}//if topBar and navlinks
-			toggleResp(mql,toggleButton);
-		});//window load
+		
+		//check if nav is not empty, which occurs only for the home page
+		if (nav.length) {
+		
+			$(window).load(function () {
+				nav.addClass('nav-resp');
+				toggleResp(mql,toggleButton);
+			});//window load
 	
-		//display toggleButton if max-width < breakpoint
-		$(window).resize(function () {
-			toggleResp(mql, toggleButton);
-		});//on window resize function
+			//display toggleButton if max-width < breakpoint
+			$(window).resize(function () {
+				toggleResp(mql, toggleButton);
+			});//on window resize function
 
-		//toggle .is-open class when clicking on menu-toggle button
-		toggleButton.on('click', function() {
-			wrapper.toggleClass('is-open');
-			main.toggleClass('pointer');
-		});//menu-toggle on click
+			//toggle .is-open class when clicking on menu-toggle button
+			toggleButton.on('click', function() {
+				wrapper.toggleClass('is-open');
+				main.toggleClass('pointer');
+			});//menu-toggle on click
 
-		//dismiss opened side menu when clicking outside of menu
-		$(document).on('click', function(event) {
-		    if ($(event.target).closest(main).length && 
-			!$(event.target).is(toggleButton)) {
-				if (wrapper.hasClass('is-open')) {
-					wrapper.removeClass('is-open');
-					main.removeClass('pointer');
-				}//if wrapper is-open, remove class
-			}//if event.target closest and != toggle
-		});//document on click
+			//dismiss opened side menu when clicking outside of menu
+			$(document).on('click', function(event) {
+			    if ($(event.target).closest(main).length && 
+				!$(event.target).is(toggleButton)) {
+					if (wrapper.hasClass('is-open')) {
+						wrapper.removeClass('is-open');
+						main.removeClass('pointer');
+					}//if wrapper is-open, remove class
+				}//if event.target closest and != toggle
+			});//document on click
+		}//if nav is not empty
 	}//if html has class 3d transform and 
-	//browser supports matchMedia
+	//browser supports match
 		
 }(jQuery));//close immediately invoked function expression
 
