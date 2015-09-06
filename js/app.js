ac3b1cc4298b15e892a30bfb842154d0d2c4df9c
Pablo Perez <pdperez@outlook.com>
2015-09-06 06:59:39 -0700

    made changes to read-more js to fade text in and out and avoid css column reflow on slide toggle

--- js/app.js
+++ js/app.js
@@ -1,106 +1,53 @@
 
-(function ($) {
-	
-    "use strict";
-   
-    var main = $('#main'),
-		toggleButton = $('#menu-toggle'),
-		closeButton = $('#menu-close'),
-		wrapper = $('#wrapper'),
-		nav = $('#navigation'),
-		html = $('html'),
-		mediaQuery = 'screen and (max-width: 44.44444em)',//responsive menu breakpoint
-		mql = window.matchMedia(mediaQuery);//media query list
-		
-	//function allows to toggle an element if a media
-	//query list is true or false
-	function toggleResp(mediaQueryList, element) {
-	    if (mediaQueryList.matches) {
-			element.show();
-		}//display element if breakpoint is met
-		else {
-			element.hide();
-		}//hide element if breakpoint is not met
-	}
-	
-	//if modernizr detects csstransforms3d, 
-	// and if browser supports matchMedia, 
-	//then add off-canvas menu
-	if (html.hasClass('csstransforms3d') && (window.matchMedia)) { 
-	
-		//add responsive classes to nav and navLinks elements
-		//menu only adopts responsive form if javascript has loaded 
-		//and added these classes
-		
-		//check if nav is not empty, which occurs only for the home page
-		if (nav.length) {
-		
-			$(window).load(function () {
-				nav.addClass('nav-resp');
-				toggleResp(mql,toggleButton);
-				toggleResp(mql,closeButton);
-			});//window load
-	
-			//display toggleButton if max-width < breakpoint
-			$(window).resize(function () {
-				toggleResp(mql, toggleButton);
-				toggleResp(mql, closeButton);
-			});//on window resize function
-
-			//toggle .is-open class when clicking on menu-toggle button
-			//also add pointer class to main
-			toggleButton.on('click', function () {
-				wrapper.toggleClass('is-open');
-				main.toggleClass('pointer');
-			});//menu-toggle button on click
-
-			//toggle .is-open class when clicking on menu-close button
-			//also add pointer class to main
-			closeButton.on('click', function () {
-				wrapper.toggleClass('is-open');
-				main.toggleClass('pointer');
-			});//menu-close button on click
-
-			//dismiss opened side menu when clicking outside of menu
-			$(document).on('click', function(event) {
-			    if ($(event.target).closest(main).length && 
-				!$(event.target).is(toggleButton)) {
-					if (wrapper.hasClass('is-open')) {
-						wrapper.removeClass('is-open');
-						main.removeClass('pointer');
-					}//if wrapper is-open, remove class
-				}//if event.target closest and != toggle
-			});//document on click
-		}//if nav is not empty
-	}//if html has class 3d transform and 
-	//browser supports match
-		
-}(jQuery));//close immediately invoked function expression
-
-;(function($){
+( function( $ ){
 	
 	"use strict";
-	
-	var moreButton = $('.read-more-button'),//array containing all .read-more-button 
-		more = $('.read-more'),//array containing all .read-more divs
-		text = moreButton.html(),//.read-more-button inner html, should be "Read more"
-		altText = moreButton.data('alt-text');//alternate text to be set after displaying .read-more
-	
-		$(window).load(function () {
-			moreButton.each(function () {
-				$(this).toggleClass("hidden");
-			});//remove "hidden" class from all read-more buttons
+	var moreButton = $('.read-more-button'),//array containing all .read-more-buttons 
+	text = moreButton.html(),//.read-more-button inner html, should be "Read more"
+	altText = moreButton.data('alt-text'),//alternate text to be set after displaying .read-more
+	animSpeed = 200;//animation speed
+
+		//on first load, hide read-more buttons
+		$( window ).load( function () {
+			moreButton.each( function () {
+				$( this ).toggleClass( "hidden" );//remove "hidden" class from all read-more buttons
+				$( this ).next( '.read-more' ).children().fadeOut();//prepare text inside
+				//read more sections by fading it out on first load
+			});
 		});//on window load event
 		
-		moreButton.click(function (event) {
-			var target = $(event.target);
-			target.next('.read-more').slideToggle("slow",function () {
-				if (target.html() === text) {
-					target.html(altText);
-				} else {
-					target.html(text);
-				}//set .read-more-button text to either text or altText
-			});//slideToggle the .read-more div
+		//on a click event of a read-more button: 
+		//	if section is hidden: slide toggle the read-more section and 
+		//		fade text in. Text has to be faded in after slidetoggle is complete
+		//		to avoid reflow of CSS columns caused by changing height of containing 
+		//		element during slidetoggle animation
+		//	if section is showing: fade text out, use a promise to check when fadeOut 
+		//		is complete, then slidetoggle. The promise helps avoid a problem of the
+		//		slidetoggle animation bouncing and not completing
+		//	html text of the read-more button is changed on each click
+		 
+		moreButton.click( function ( event ) {
+			
+			var target = $( event.target ),
+			more = target.next( '.read-more' ),
+			moreChildren = more.children();
+
+			if ( target.html() === text ) {
+				more.slideToggle( animSpeed).promise().done(
+					function () {
+						target.html( altText );
+						moreChildren.fadeIn( animSpeed );
+				});
+			} else {
+				moreChildren.fadeOut( animSpeed ).promise().done(
+					function () {
+						more.slideToggle (animSpeed , function () {
+							target.html( text );	
+						});
+					}
+				);
+			}
 		});//on moreButton click event
-}(jQuery));
+		
+}( jQuery ));
 
--- js/app/read-more.js
+++ js/app/read-more.js
@@ -1,27 +1,53 @@
-(function($){
+
+( function( $ ){
 	
 	"use strict";
-	
-	var moreButton = $('.read-more-button'),//array containing all .read-more-button 
-		more = $('.read-more'),//array containing all .read-more divs
-		text = moreButton.html(),//.read-more-button inner html, should be "Read more"
-		altText = moreButton.data('alt-text');//alternate text to be set after displaying .read-more
-	
-		$(window).load(function () {
-			moreButton.each(function () {
-				$(this).toggleClass("hidden");
-			});//remove "hidden" class from all read-more buttons
+	var moreButton = $('.read-more-button'),//array containing all .read-more-buttons 
+	text = moreButton.html(),//.read-more-button inner html, should be "Read more"
+	altText = moreButton.data('alt-text'),//alternate text to be set after displaying .read-more
+	animSpeed = 200;//animation speed
+
+		//on first load, hide read-more buttons
+		$( window ).load( function () {
+			moreButton.each( function () {
+				$( this ).toggleClass( "hidden" );//remove "hidden" class from all read-more buttons
+				$( this ).next( '.read-more' ).children().fadeOut();//prepare text inside
+				//read more sections by fading it out on first load
+			});
 		});//on window load event
 		
-		moreButton.click(function (event) {
-			var target = $(event.target);
-			target.next('.read-more').slideToggle("slow",function () {
-				if (target.html() === text) {
-					target.html(altText);
-				} else {
-					target.html(text);
-				}//set .read-more-button text to either text or altText
-			});//slideToggle the .read-more div
+		//on a click event of a read-more button: 
+		//	if section is hidden: slide toggle the read-more section and 
+		//		fade text in. Text has to be faded in after slidetoggle is complete
+		//		to avoid reflow of CSS columns caused by changing height of containing 
+		//		element during slidetoggle animation
+		//	if section is showing: fade text out, use a promise to check when fadeOut 
+		//		is complete, then slidetoggle. The promise helps avoid a problem of the
+		//		slidetoggle animation bouncing and not completing
+		//	html text of the read-more button is changed on each click
+		 
+		moreButton.click( function ( event ) {
+			
+			var target = $( event.target ),
+			more = target.next( '.read-more' ),
+			moreChildren = more.children();
+
+			if ( target.html() === text ) {
+				more.slideToggle( animSpeed).promise().done(
+					function () {
+						target.html( altText );
+						moreChildren.fadeIn( animSpeed );
+				});
+			} else {
+				moreChildren.fadeOut( animSpeed ).promise().done(
+					function () {
+						more.slideToggle (animSpeed , function () {
+							target.html( text );	
+						});
+					}
+				);
+			}
 		});//on moreButton click event
-}(jQuery));
+		
+}( jQuery ));
 
